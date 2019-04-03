d2b7f55e664cd9c32ab464a106c057a124375545
Not Iain Moncrief <notiainmoncrief@notgmail.com>
2019-04-03 10:16:06 -1000

    created download script. need to package everything still. have not tested download scipt.

---
+++ downloadfreshcopy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Created by Iain Moncrief on Wednesday, April 3rd, 2019
+# Iain Moncrief <iainmoncrief@gmail.com>
+
+rm MagicMirror.tar.gz || echo "Already clean. OK! Proceeding..."
+curl -O https://raw.githubusercontent.com/oceanit-magicmirror-unofficial/Oceanit-MagicMirror-HomeDirectory/master/Desktop/.imessedup/backup/MagicMirror.tar.gz
+rm Desktop/.imessedup/backup/MagicMirror.tar.gz || echo "Backup file already deleted. OK!"
+mv MagicMirror.tar.gz Desktop/.imessedup/backup/MagicMirror.tar.gz && echo "Good to go! Backup archive has been updated."
+
+echo "Unpacking archive..."
+cd Desktop/.imessedup/backup && tar -xzf MagicMirror.tar.gz
+echo "All done"
